f2bb203c5413cbc8d615cdcf705b79b2fd16704e
yeming on youyu <492851378@qq.com>
2021-08-26 14:39:21 +0800

    implement

--- djackets_django/product/serializers.py
+++ djackets_django/product/serializers.py
@@ -7,10 +7,24 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = (
+            "id",
             "name",
             "description",
             "price",
             "get_absolute_url",
             "get_image",
             "get_thumbnail"
+        )
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True)
+    class Meta:
+        model = Category
+        fields = (
+            'id',
+            "name",
+            "slug",
+            "get_absolute_url",
+            "products"
         )
--- djackets_vue/src/views/Product.vue
+++ djackets_vue/src/views/Product.vue
@@ -57,6 +57,8 @@ export default {
                 .get(`/api/v1/products/${category_slug}/${product_slug}/`)
                 .then(response => {
                     this.product = response.data
+
+                    document.title = this.product.name + ' | Djankets'
                 })
                 .catch(error => {
                     console.log(error)
